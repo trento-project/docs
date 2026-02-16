0946f75e3989dc2f937aa6e9db4547ff42a66fd2
Jurgen <jurgen.goldschmidt@suse.com>
2026-02-16 13:34:25 +0200

    Mobile Search Improvement (#115)
    * Changes search elements for mobile layouts and adds styles
    
    * Adds functionality to toggle the display of search for mobile layouts
    
    * Adds eos-icons package
    
    * Adds eos-icons as a font and includes it in the gulp build
    
    * Adds ui-bundle.zip file

--- trento-docs-site-ui/src/js/05-mobile-navbar.js
+++ trento-docs-site-ui/src/js/05-mobile-navbar.js
@@ -5,16 +5,30 @@
   if (!navbarBurger) return
   navbarBurger.addEventListener('click', toggleNavbarMenu.bind(navbarBurger))
 
+  var searchField = document.getElementById('search-wrapper')
+  var searchClose = document.getElementById('search-close')
+  var searchToggle = document.querySelector('.search-toggle')
+  if (!searchField) return
+  searchClose.addEventListener('click', toggleSearch)
+  searchToggle.addEventListener('click', toggleSearch)
+
   function toggleNavbarMenu (e) {
     e.stopPropagation() // trap event
+    !searchField.classList.contains('hidden') && toggleSearch(e)
     document.documentElement.classList.toggle('is-clipped--navbar')
-    navbarBurger.setAttribute('aria-expanded', this.classList.toggle('is-active'))
-    var menu = document.getElementById(this.getAttribute('aria-controls') || this.dataset.target)
+    navbarBurger.setAttribute('aria-expanded', navbarBurger.classList.toggle('is-active'))
+    var menu = document.getElementById(navbarBurger.getAttribute('aria-controls') || navbarBurger.dataset.target)
     if (menu.classList.toggle('is-active')) {
       menu.style.maxHeight = ''
       var expectedMaxHeight = window.innerHeight - Math.round(menu.getBoundingClientRect().top)
       var actualMaxHeight = parseInt(window.getComputedStyle(menu).maxHeight, 10)
       if (actualMaxHeight !== expectedMaxHeight) menu.style.maxHeight = expectedMaxHeight + 'px'
     }
+  }
+
+  function toggleSearch (e) {
+    e.stopPropagation()
+    navbarBurger.classList.contains('is-active') && toggleNavbarMenu(e)
+    searchField.classList.toggle('hidden')
   }
 })()
